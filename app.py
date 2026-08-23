88f46c332eb69559f4de9529eef058f555bfd26f
murad9981 <abdallahsarfaraz123@gmail.com>
2026-08-23 02:28:39 -0700

    Update image source in app.py for house prediction

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 model = joblib.load('hpr.pkl')
 st.title('House Price Prediction')
-st.image("h.jpg")
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJX8tOwL9Xq6gCfBLx7GO62fXroawmrPGurjwhQhR_dw&s=10")
 Area = st.number_input('SQUARE_FT')
 
 if st.button('Predict'):
