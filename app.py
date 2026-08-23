866ec172a763ef43121036b033dd55142289eb66
murad9981 <abdallahsarfaraz123@gmail.com>
2026-08-23 01:46:39 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+model = joblib.load('hpr.pkl')
+st.title('House Price Prediction')
+Area = st.number_input('SQUARE_FT')
+
+if st.button('Predict'):
+    data = [[Area]]
+    prediction = model.predict(data)
+
+    st.write('Predicted Price:', prediction[0])
