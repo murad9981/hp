d4994ed0a5b87fe09467b00136285064b3bf99fd
murad9981 <abdallahsarfaraz123@gmail.com>
2026-08-23 02:17:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 model = joblib.load('hpr.pkl')
 st.title('House Price Prediction')
-st.image('https://www.google.com/search?q=house&sca_esv=ae66a29e8f623646&udm=2&biw=1536&bih=730&sxsrf=APpeQnvh5w6MsZ-V1t5aTbZY59GUvhyCKQ%3A1787476175084&ei=z7iKatnbBKydseMPruCOmQE&oq=house&gs_lp=Egtnd3Mtd2l6LWltZyIFaG91c2UqAggAMg0QABiABBiKBRhDGLEDMggQABiABBixAzIIEAAYgAQYsQMyCxAAGIAEGLEDGIMBMggQABiABBixAzIFEAAYgAQyBRAAGIAEMgUQABiABDIIEAAYgAQYsQMyBRAAGIAESINAULkxWLkxcAN4AJABAJgBgQGgAYEBqgEDMC4xsAEAuAEByAEA-AEBmAIEoAKWAcICBhAAGAcYHsICCBAAGAcYHhgKwgIHEAAYgAQYCpgDAOIDBBgAIF3iAwQYACBe4gMEGAAgX-IDBBgAIGDiAwQYACBh4gMEGAAgYogGAZIHAzMuMaAH1gSyBwMwLjG4B4cBwgcDMi00yAcQgAgB&sclient=gws-wiz-img#sv=CAMSURoyKhBlLTdWSFcwUWFwY3ZGTzJNMg43VkhXMFFhcGN2Rk8yTToOV1lwQ1VTQ05vUXFiWE0gBCoXCgFzEhBlLTdWSFcwUWFwY3ZGTzJNGAEwARgHIKG-0K0DSggQARgBIAEoAQ')
+st.image=('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJX8tOwL9Xq6gCfBLx7GO62fXroawmrPGurjwhQhR_dw&s=10')
 Area = st.number_input('SQUARE_FT')
 
 if st.button('Predict'):
